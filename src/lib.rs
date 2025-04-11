13ec5166a2b12aaeeebe50e3318b8938f8fd99ad
nicolas gailly <nikkolasg@gmail.com>
2025-04-11 13:39:11 +0200

    working with external translator

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,40 @@
 #![feature(min_specialization)]
 
-use std::fmt::Debug;
+use std::{any::Any, fmt::Debug, marker::PhantomData};
 
 /// FRAMEWORK LOGIC TRAIT AND IMPLEMENTATION: 
 /// 
 // Single trait for all operations
-trait Operation<N> :  Debug {
+trait Operation<N>: Debug + Any {
     fn apply(&self) -> N;
     
     fn discretize(&self) -> Option<Box<dyn Operation<u32>>> {
         None
+    }
+}
+
+trait ProvableOperation<Field> : Operation<u32> {
+    fn prove(&self);
+}
+
+trait IntoProvable {
+    fn into_provable<Field>(&self) -> Box<dyn ProvableOperation<Field>>;
+}
+
+struct ProvableTranslator<Field> {
+    _field: PhantomData<Field>,
+}
+
+impl<Field> ProvableTranslator<Field> {
+    fn new() -> Self {
+        Self { _field: PhantomData }
+    }
+    fn translate(op: Box<dyn Operation<u32>>) -> Box<dyn ProvableOperation<Field>> {
+        let opa = op.as_ref() as &dyn Any;
+        match opa.downcast_ref::<DoubleOp<u32>>() {
+            Some(i) => i.into_provable(),
+            None => panic!("Operation is not a DoubleOp")
+        }
     }
 }
 
@@ -28,9 +53,13 @@ impl OpTrace<f32> {
 }
 
 impl OpTrace<u32> {
-    fn prove_it(&self) {
-        println!("Proving OpTrace: {:?}", self.ops);
+    fn to_provable_trace<Field>(self) -> ProveTrace<Field>{
+        ProveTrace {  ops: ProvableTranslator::translate(self.ops) }
     }
+}
+
+struct ProveTrace<Field> {
+    ops: Box<dyn ProvableOperation<Field>>,
 }
 /// 
 /// USER DEFINED IMPLEMENTATION
@@ -38,7 +67,10 @@ impl OpTrace<u32> {
 struct DoubleOp<N>(N);
 
 // Base implementation for all types with default methods
-impl<N> Operation<N> for DoubleOp<N> where N: std::ops::Add<Output=N> + Copy + Debug {
+impl<N> Operation<N> for DoubleOp<N> 
+where 
+    N: std::ops::Add<Output = N> + Copy + Debug + 'static
+{
     default fn apply(&self) -> N {
         self.0 + self.0
     }
@@ -54,6 +86,25 @@ impl Operation<f32> for DoubleOp<f32> {
         Some(Box::new(DoubleOp(self.0.abs() as u32)))
     }
 }
+
+impl IntoProvable for DoubleOp<u32> {
+    fn into_provable<Field>(&self) -> Box<dyn ProvableOperation<Field>> {
+        Box::new(DoubleOp(self.0))
+    }
+}
+impl<Field> ProvableOperation<Field> for DoubleOp<u32> {
+    fn prove(&self) {
+        println!("Proving DoubleOp: {:?}", self.0);
+    }
+}
+
+
+
+//impl<T> IntoProvable<u32> for Box<T> where T: IntoProvable<u32> {
+//    fn into_provable(&self) -> Box<dyn ProvableOperation<u32>> {
+//        self.as_ref().into_provable()
+//    }
+//}
 
 
 #[cfg(test)]
@@ -71,6 +122,7 @@ mod tests {
         let trace = OpTrace { ops: boxed_op };
         let discrete_trace = trace.discretize();
         assert_eq!(discrete_trace.ops.apply(), 4);
-        discrete_trace.prove_it();
+        let provable_trace: ProveTrace<f32> = discrete_trace.to_provable_trace();
+        provable_trace.ops.prove();
     }
 }
